bf645795442ce55a452921591603ae804cbc2cc5
Charlie Remote <charlieremote@Ann-Hatchs-iMac.local>
2017-03-23 11:22:24 -0500

    implemented lucky # plus fortune

--- main.py
+++ main.py
@@ -15,10 +15,45 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    # list of possible fortunes
+    fortunes = [
+        "whoops...eternal damnation",
+        "long weekend in 6th circle of hell (with the heretics)",
+        "vacation for 3: you, Dante and Virgil visit the inferno",
+        "ouch...a daily ripping out of your regenerating liver by a bald eagle",
+        "several ungentle slaps on the wrist",
+        "forever haunted by the ghost of sammy haggar"
+
+    ]
+
+    # randomly select one of the fortunes
+    index = random.randint(0,len(fortunes)-1)
+
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!!!!!!!!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = getRandomFortune()
+        fortune_sentence = "Your fortune: <b>" + fortune + "</b>"
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = random.randint(1,100)
+        number_sentence = 'Your lucky number: <strong> a billion</strong>' #+ str(lucky_number)
+        number_paragraph = "<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href='.'><button> Another cookie, please </button> </a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+
+        self.response.write(content)
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
